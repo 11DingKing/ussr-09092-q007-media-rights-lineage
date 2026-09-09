0ca1dbe2156e60f723988cec2e5cf98311700e6e
ding <ding@ding.com>
2026-09-09 23:40:49 +0800

    重建09092 q007基础工程

--- app/models/asset.rb
+++
@@ -1,8 +0,0 @@
-class Asset
-  attr_reader :digest, :parent_digest
-
-  def initialize(digest:, parent_digest: nil)
-    @digest = digest
-    @parent_digest = parent_digest
-  end
-end
--- test/models_test.rb
+++
@@ -1,9 +0,0 @@
-require "minitest/autorun"
-require_relative "../app/models/asset"
-
-class AssetTest < Minitest::Test
-  def test_derivative_points_to_parent
-    asset = Asset.new(digest: "child", parent_digest: "source")
-    assert_equal "source", asset.parent_digest
-  end
-end
